45a0b78297c9a7d0e957dc7cd2c2d2f0ad7602ce
sheldenr3 <sheldenr3@gmail.com>
2025-04-09 15:18:50 +0200

    Please enter the commit message for your changes. Lines starting

--- src/components/home.tsx
+++
@@ -1,7 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-function Home() {
-  return <Navigate to="/" replace />;
-}
-
-export default Home;
--- supabase/migrations/20240620000001_update_study_buddy_table.sql
+++
@@ -1,4 +0,0 @@
--- Add character_id and show_floating_buddy columns to study_buddy table
-ALTER TABLE IF EXISTS study_buddy
-ADD COLUMN IF NOT EXISTS character_id TEXT,
-ADD COLUMN IF NOT EXISTS show_floating_buddy BOOLEAN DEFAULT TRUE;
